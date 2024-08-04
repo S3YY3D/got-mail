9fa60836d661a7bfb13c708e0378a20f909306d8
Seyyed Amir Mahdi Anvari <s.anvari@email.kntu.ac.ir>
2024-08-04 12:34:44 +0330

    delete unnececery migration files and creating routes and controllers

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function create()
+    {
+        return view('contact');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        $contact = new Contact();
+        $contact->first_name = $request->first_name;
+        $contact->last_name = $request->last_name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->save();
+
+        return back()->with('success', 'Thanks for contacting us!');
+    }
+}
